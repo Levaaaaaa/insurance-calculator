a8f90041f57f664c0018fd18a0a5ad416f085ad5
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-06-25 13:20:53 +0300

    add_flowers_project

---
+++ Flowers_project/Flowers_project/Camomile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowers_project
+{
+    internal class Camomile : BaseFlower
+    {
+        private readonly int countOfPetal;
+        public Camomile(string name, int length, int freshness, double cost, string color, int countOfPetal) : base(name, length, freshness, cost, color)
+        {
+            this.countOfPetal = countOfPetal;
+            type = "camomile";
+            //_ = strForFile.Insert(0, type);
+            //this.strForFile += "~" + countOfPetal.ToString();
+        }
+
+        public Camomile(string[] str) : base(str)
+        {
+            type = str[0];
+            countOfPetal = Convert.ToInt32(str[6]);
+        }
+        public int getCountOfPetal() { return countOfPetal; }
+
+        public override string toStringForConsole()
+        {
+            return "Type: " + type + base.toStringForConsole() + "\nPetals: " + countOfPetal.ToString() + "\n";
+        }
+
+        public override string toStringForFile()
+        {
+            return type + "~" + base.toStringForFile() + "~" + countOfPetal.ToString();
+        }
+
+        public override void Write(string path)
+        {
+            StreamWriter writer = new StreamWriter(path);
+            writer.WriteLine(this.toStringForFile());
+            writer.Close();
+        }
+    }
+}
---
+++ Flowers_project/Flowers_project/ConsoleOutput.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowers_project
+{
+    internal class ConsoleOutput
+    {
+        public void outputBouquetInLine(Bouquet bouquet)
+        {
+            int countOfLines = BaseFlower.getCountOfParameters();
+            string[] outputLines = new string[countOfLines];
+            string[,] allFlowers = new string[countOfLines, bouquet.getBouquet().Length];
+            int j = 0;
+            foreach (BaseFlower flower in bouquet.getBouquet())     //собираем двумерный массив выводимых строк, каждый внутренний массив соответствует отдельному цветку букета
+            {
+                string[] buff = flower.toStringForConsole().Split("\n").SkipLast(1).ToArray();
+                for (int i = 0; i < countOfLines; i++)
+                {
+                    allFlowers[i, j] = buff[i];
+                }
+                j++;
+            }
+
+            string line = "";                       //склеиваем строки соответствующих параметров всех цветов букета
+            for (int i = 0; i < countOfLines; i++)
+            {
+                line = "";
+                for (int k = 0; k < bouquet.getBouquet().Length; k++)
+                {
+                    if (i == 7)
+                    {
+                        int b = 0;
+                    }
+                    line += allFlowers[i, k];
+                    int countOfSpaces = maxLengthInStrings(allFlowers, k) - allFlowers[i, k].Length;    //высчитываем максимальный отступ
+                    for (int p = 0; p < countOfSpaces; p++)
+                    {
+                        line += " ";
+                    }
+
+                    line += " | ";
+                }
+                outputLines[i] = line.Replace('\r', ' ');
+            }
+
+            print(outputLines);
+        }
+
+        private static int maxLengthInStrings(string[,] array, int j)
+        {
+            int maxLength = array[0, j].Length;
+            for (int i = 0; i < BaseFlower.getCountOfParameters(); i++)
+            {
+                if (array[i,j].Length > maxLength) maxLength = array[i, j].Length;
+            }
+
+            return maxLength;
+        }
+
+        private static void print(string[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
